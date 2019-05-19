c591616a898ceb86c1d0aedb0e0773ea5e9fdeec
Naif Mohamed <naif.mohamed@pension.gov.mv>
2019-05-19 14:54:05 +0500

    minor bug fix

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -16,7 +16,7 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-dhivehi-field', __DIR__ . '/../dist/js/jtk.js');
+            Nova::script('nova-dhivehi-field-jtk', __DIR__ . '/../dist/js/jtk.js');
             Nova::script('nova-dhivehi-field', __DIR__ . '/../dist/js/field.js');
             Nova::style('nova-dhivehi-field', __DIR__ . '/../dist/css/field.css');
             // Nova::script('dhivehi-text-field', __DIR__ . '/../dist/js/jtk.js');
