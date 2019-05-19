ed210a1c9481d76c553edb3eb7df8c9a27486a8d
Naif Mohamed <naifmhd@gmail.com>
2019-05-19 10:16:26 +0000

    Apply fixes from StyleCI

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace Ninesixtymv\NovaDhivehiField;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
 
 class FieldServiceProvider extends ServiceProvider
 {
@@ -16,9 +16,9 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-dhivehi-field-jtk', __DIR__ . '/../dist/js/jtk.js');
-            Nova::script('nova-dhivehi-field', __DIR__ . '/../dist/js/field.js');
-            Nova::style('nova-dhivehi-field', __DIR__ . '/../dist/css/field.css');
+            Nova::script('nova-dhivehi-field-jtk', __DIR__.'/../dist/js/jtk.js');
+            Nova::script('nova-dhivehi-field', __DIR__.'/../dist/js/field.js');
+            Nova::style('nova-dhivehi-field', __DIR__.'/../dist/css/field.css');
             // Nova::script('dhivehi-text-field', __DIR__ . '/../dist/js/jtk.js');
             // Nova::script('dhivehi-textarea-field', __DIR__ . '/../dist/js/field.js');
             // Nova::style('dhivehi-textarea-field', __DIR__ . '/../dist/css/field.css');
